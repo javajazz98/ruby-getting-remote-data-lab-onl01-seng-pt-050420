ae6c1f61f2d7e60c07d6095f8d37038e4582624a
jassmine tatum <jassmine.tatum@gmail.com>
2020-06-22 02:31:57 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,18 +1,16 @@
-class GetRequester
+require 'net/http'
+ require 'open-uri'
+ require 'json'
 
-  attr_accessor :url
-  def initialize(url)
-    @url = url
-  end
+ 
+ class GetRequester
+
+    def initialize(url)
+        @url = url
+    end
 
   def get_response_body
     uri = URI.parse(@url)
     response = Net::HTTP.get_response(uri)
     response.body
   end
-
-  def parse_json
-    JSON.parse(get_response_body)
-  end
-
-end
